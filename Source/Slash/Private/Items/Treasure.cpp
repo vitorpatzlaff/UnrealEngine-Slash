8ef9dc098de1151210416bc63cbbb0333702529e
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-07-04 00:22:23 -0300

    add Treasure class

---
+++ Source/Slash/Private/Items/Treasure.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Kismet/GameplayStatics.h"
+
+#include "Characters/SlashCharacter.h"
+#include "Items/Treasure.h"
+
+void ATreasure::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ASlashCharacter* SlashCharacter = Cast<ASlashCharacter>(OtherActor);
+	if (SlashCharacter) {
+		if (PickupSound) {
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				PickupSound,
+				GetActorLocation()
+			);
+		}
+
+		Destroy();
+	}
+}
