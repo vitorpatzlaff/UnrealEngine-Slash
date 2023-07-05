398822155528b0cf81541fbca5b3b7a9c70fd562
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2023-07-05 01:40:43 -0300

    add more treasures

--- Source/Slash/Private/Breakable/BreakableActor.cpp
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Components/CapsuleComponent.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 #include "Breakable/BreakableActor.h"
+#include "Items/Treasure.h"
 
 // Sets default values
 ABreakableActor::ABreakableActor()
@@ -13,6 +16,14 @@ ABreakableActor::ABreakableActor()
 	SetRootComponent(GeometryCollection);
 	GeometryCollection->SetGenerateOverlapEvents(true);
 	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	Capsule->SetupAttachment(GetRootComponent());
+	Capsule->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	Capsule->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Block);
+
+	HasBeenHit = false;
 }
 
 void ABreakableActor::BeginPlay()
@@ -29,6 +40,34 @@ void ABreakableActor::Tick(float DeltaTime)
 
 void ABreakableActor::GetHit_Implementation(const FVector& ImpactPoint)
 {
+	FVector Location = GetActorLocation();
 
+	if (BreakSound) {
+		UGameplayStatics::PlaySoundAtLocation(
+			this,
+			BreakSound,
+			Location
+		);
+	}
+	// can be in the blueprint
+
+	UWorld* World = GetWorld();
+
+	if (!HasBeenHit && World && TreasureClasses.Num() > 0) {
+		Location.Z += 75.f;
+
+		const int8 Selection = FMath::RandRange(0, TreasureClasses.Num() - 1);
+		World->SpawnActor<ATreasure>(TreasureClasses[Selection], Location, GetActorRotation());
+
+		HasBeenHit = true;
+
+		Capsule->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+		//SetLifeSpan(3.f);
+		/* 
+			This two above things can be done on the OnChaosBreakEvent.
+			Further info, the GetHit_Implementation captures only hits from the player, now this event captures anything
+			that breaks the "GeometryCollection"
+		*/
+	}
 }
 
--- Source/Slash/Public/Breakable/BreakableActor.h
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -24,9 +24,20 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+	TArray<TSubclassOf<class ATreasure>> TreasureClasses;
+
+	UPROPERTY(EditAnywhere, Category = Sounds)
+	USoundBase* BreakSound;	
+
+	bool HasBeenHit;
 
 };
   
